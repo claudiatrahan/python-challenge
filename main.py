4fee7153f0ba5538b92cd5861e09e2a8e55d9426
claudiatrahan <claudia.trahan@gmail.com>
2020-03-17 12:28:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#dependencies
+import csv
+import os
 
+# input
+csvpath = os.path.join('..','Raw_Data','election_data.csv')
+
+poll ={}
+total_votes = 0
+candidates = []
+candidates_list = []
+candidates_votes = []
+num_votes = []
+total_votes = 0
+
+with open(csvpath) as election_file:
+    reader = csv.reader(election_file, delimiter=',')
+    print(reader)
+    csv_header = next(reader)
+    for line in reader:
+        total_votes += 1
+        candidates_votes.append(line[2])
